204bbafd422f4a7527a3f82693b883f0d8651243
E3Prime <primesoftware3@gmail.com>
2026-01-07 14:19:40 -0700

    refactored game functionality and slightly made adjustments for styling and structure.

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function initialize() {
   const gameBoard = /** @type {HTMLElement} */ (document.getElementById('gameBoard'));
-  const gameDecision = /** @type {HTMLElement} */ (document.getElementById('gameDecision'));
+  const gameHands = /** @type {NodeListOf<HTMLButtonElement>} */ (gameBoard.querySelectorAll('button'));
+  const gameDecisionBoard = /** @type {HTMLElement} */ (document.getElementById('gameDecisionBoard'));
   const gameVerdict = /** @type {HTMLElement} */ (document.getElementById('gameVerdict'));
-  const playAgainBtn = /** @type {HTMLButtonElement} */ (document.getElementById('playAgain'));
-  const rulesBtn = /** @type {HTMLButtonElement} */ (document.getElementById('rules'));
-  const infoRules = /** @type {HTMLElement} */ (document.getElementById('infoRules'));
+  const replayBtn = /** @type {HTMLButtonElement} */ (gameVerdict.lastElementChild);
+  const openRulesBtn = /** @type {HTMLButtonElement} */ (document.getElementById('openRules'));
   const closeRulesBtn = /** @type {HTMLButtonElement} */ (document.getElementById('closeRules'));
+  const rules = /** @type {HTMLElement} */ (closeRulesBtn.parentElement);
   const scoreElem = /** @type {HTMLElement} */ (document.getElementById('score'));
+  const [playerHand, computerHand] = /** @type {HTMLButtonElement[]} */ (Array.from(gameDecisionBoard.querySelectorAll('button')));
+  const targetClassNames = ['btn-paper', 'btn-scissors', 'btn-rock'];
   let score = 0;
 
   const rpsOutcomes = new Map([
@@ -41,94 +44,97 @@ function initialize() {
     ],
   ]);
 
-  /** @param {Event} e */
-  const performAction = (e) => {
+  /** @param {PointerEvent} e */
+  const performAction = async (e) => {
     const target = e.target;
     if (!(target instanceof HTMLButtonElement)) return;
+    gameHands.forEach((gh) => (gh.disabled = true));
     const playerChoice = target.id;
     const computerChoice = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
-    hideGameBoardAndRevealGameDecision();
-    revealHands(playerChoice, computerChoice);
+    await hideBoard(gameBoard);
+    await revealBoard(gameDecisionBoard);
+    await revealHands(targetClassNames, playerHand, computerHand, playerChoice, computerChoice);
+    determineWinner(playerHand, computerHand, playerChoice, computerChoice);
+  };
+
+  const replay = async () => {
+    await hideBoard(gameDecisionBoard);
+    gameVerdict.style.visibility = 'hidden';
+    playerHand.classList.remove(...targetClassNames, 'winner');
+    computerHand.classList.remove(...targetClassNames, 'winner', 'btn-hand');
+    computerHand.classList.add('mystery-hand');
+    computerHand.replaceChildren();
+    revealBoard(gameBoard);
+    gameHands.forEach((gh) => (gh.disabled = false));
   };
 
   /**
-   * @param {string} playerChoice
-   * @param {string} computerChoice
-   */
-  const revealHands = async (playerChoice, computerChoice) => {
-    /** @type {NodeListOf<HTMLButtonElement>} */
-    const [playerHand, computerHand] = gameDecision.querySelectorAll('.pick-container > :first-child');
-    const playerHandImg = /** @type {HTMLImageElement} */ (playerHand.firstElementChild);
-    const computerHandImg = /** @type {HTMLImageElement} */ (computerHand.firstElementChild);
-    const targetClassNames = ['btn-paper', 'btn-scissors', 'btn-rock'];
-    playerHand.classList.remove(...targetClassNames);
-    computerHand.classList.remove(...targetClassNames);
-    playerHand.classList.add('btn-' + playerChoice);
-    computerHand.classList.add('btn-' + computerChoice);
-    playerHandImg.src = `images/icon-${playerChoice}.svg`;
-    computerHandImg.src = `images/icon-${computerChoice}.svg`;
-    await sleep(1300);
-    playerHand.style.opacity = '1';
-    await sleep(1300);
-    computerHand.style.opacity = '1';
-    await sleep(1300);
-    verdict(playerChoice, computerChoice, playerHand, computerHand);
-  };
-
-  /**
-   * @param {string} playerChoice
-   * @param {string} computerChoice
    * @param {HTMLButtonElement} playerHand
    * @param {HTMLButtonElement} computerHand
+   * @param {string} playerChoice
+   * @param {string} computerChoice
    */
-  const verdict = (playerChoice, computerChoice, playerHand, computerHand) => {
-    const result = rpsOutcomes.get(playerChoice)?.get(computerChoice);
-    const winner = /** @type {HTMLElement} */ (gameVerdict.firstElementChild);
+  const determineWinner = (playerHand, computerHand, playerChoice, computerChoice) => {
+    const outcome = rpsOutcomes.get(playerChoice)?.get(computerChoice);
+    const verdict = /** @type {HTMLElement} */ (gameVerdict.firstElementChild);
 
-    if (result === 'win') {
-      winner.textContent = 'YOU WIN!';
-      playerHand.classList.add('winner');
+    if (outcome === 'win') {
       ++score;
+      verdict.textContent = 'YOU WIN!';
+      playerHand.classList.add('winner');
       scoreElem.textContent = score.toString();
-    } else if (result === 'lose') {
-      winner.textContent = 'YOU LOSE';
+    } else if (outcome === 'lose') {
+      verdict.textContent = 'YOU LOSE!';
       computerHand.classList.add('winner');
     } else {
-      winner.textContent = 'DRAW!';
+      verdict.textContent = 'DRAW!';
     }
+
     gameVerdict.style.visibility = 'visible';
-  };
-
-  const hideGameBoardAndRevealGameDecision = async () => {
-    gameBoard.classList.add('fade-out');
-    await sleep(1000);
-    gameBoard.style.display = 'none';
-    gameDecision.style.display = 'flex';
-    gameDecision.classList.add('fade-in');
-    await sleep(1000);
-  };
-
-  const hideGameDecisionAndRevealGameBoard = async () => {
-    /** @type {NodeListOf<HTMLButtonElement>} */
-    const [playerHand, computerHand] = gameDecision.querySelectorAll('.pick-container > :first-child');
-    playerHand.classList.remove('winner');
-    computerHand.classList.remove('winner');
-    gameDecision.classList.remove('fade-in');
-    gameDecision.classList.add('fade-out');
-    playerHand.style.opacity = '0';
-    computerHand.style.opacity = '0';
-    gameVerdict.style.visibility = 'hidden';
-    await sleep(1000);
-    gameDecision.style.display = 'none';
-    gameDecision.classList.remove('fade-out');
-    await sleep(1000);
-    gameBoard.classList.remove('fade-out');
-    gameBoard.classList.add('fade-in');
-    gameBoard.style.display = 'flex';
+    replayBtn.disabled = false;
   };
 
   gameBoard.addEventListener('click', performAction);
-  playAgainBtn.addEventListener('click', hideGameDecisionAndRevealGameBoard);
-  rulesBtn.addEventListener('click', () => infoRules.classList.add('reveal-rules'));
-  closeRulesBtn.addEventListener('click', () => infoRules.classList.remove('reveal-rules'));
+  replayBtn.addEventListener('click', replay);
+  openRulesBtn.addEventListener('click', () => rules.classList.add('reveal'));
+  closeRulesBtn.addEventListener('click', () => rules.classList.remove('reveal'));
+}
+
+/**
+ * @param {string[]} targetClassNames
+ * @param {HTMLButtonElement} playerHand
+ * @param {HTMLButtonElement} computerHand
+ * @param {string} playerChoice
+ * @param {string} computerChoice
+ */
+async function revealHands(targetClassNames, playerHand, computerHand, playerChoice, computerChoice) {
+  const playerHandImg = /** @type {HTMLImageElement} */ (playerHand.firstElementChild);
+  playerHand.classList.remove(...targetClassNames);
+  computerHand.classList.remove(...targetClassNames);
+  playerHand.classList.add('btn-' + playerChoice);
+  playerHandImg.src = `images/icon-${playerChoice}.svg`;
+  await sleep(3000);
+  const computerHandImg = document.createElement('img');
+  computerHandImg.src = `images/icon-${computerChoice}.svg`;
+  computerHand.appendChild(computerHandImg);
+  computerHand.classList.remove('mystery-hand');
+  computerHand.classList.add('btn-hand');
+  computerHand.classList.add('btn-' + computerChoice);
+  await sleep(700);
+}
+
+/** @param {HTMLElement} board */
+async function revealBoard(board) {
+  board.classList.remove('fade-out');
+  board.classList.add('fade-in');
+  await sleep(700);
+  board.id === 'gameBoard' ? (board.style.display = 'block') : (board.style.display = 'flex');
+}
+
+/** @param {HTMLElement} board */
+async function hideBoard(board) {
+  board.classList.remove('fade-in');
+  board.classList.add('fade-out');
+  await sleep(700);
+  board.style.display = 'none';
 }
